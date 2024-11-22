8cda083ae5892e3f4ee5c0e5a1b5fc2cca9c4211
eemarishkin <ee.marishkin@yandex.ru>
2024-11-22 20:57:17 +0400

    berloga-1-STYLE

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function Footer(){
+    return(
+        <footer>
+           Все права не защищены &copy;
+        </footer>
+    )
+} 
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export function Header(){
+    return(
+        <header>
+           <div>
+             <span className="logo">страшная берлога</span>
+           </div>
+           <ul className="nav">
+             <li>про нас</li>
+             <li>контакты</li>
+             <li>кабинет</li>
+           </ul>
+           <div className="presentstion"></div>
+        </header>
+    )
+} 
